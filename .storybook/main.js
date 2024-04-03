bde850c802d97db847750058515af3e16f9c6530
adesh <>
2024-04-03 17:14:01 +0530

    added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+import { join, dirname } from "path";
+import webpackConfig from "./webpack.config";
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../packages/**/stories/**/*.mdx",
+    "../packages/**/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  webpack: webpackConfig,
+  framework: {
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {
+      
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
